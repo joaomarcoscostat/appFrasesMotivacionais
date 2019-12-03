46907c739a5c311cd54f1937760c54ee9de2db0d
joaomarcoscostat <joaomarcoscosta13@hotmail.com>
2019-12-02 21:02:04 -0300

    progress bar

--- appMotivacional/app/src/main/java/com/example/appmotivacional/MainActivity.java
+++ appMotivacional/app/src/main/java/com/example/appmotivacional/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.appmotivacional;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.appmotivacional.model.Frases;
@@ -24,21 +35,22 @@ public class MainActivity extends AppCompatActivity {
     private Frases Frase;
     private Button btnMudar;
     private ImageView btnShare;
-
-
+    private ProgressBar progressBar;
+    private  TextView Resultado;
+    public static int id=880;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getFrases();
         btnMudar = findViewById(R.id.btnMudar);
-        btnMudar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getFrases();
-            }
-        });
+        progressBar=findViewById(R.id.progressBar);
         btnShare=findViewById(R.id.btnShare);
+         Resultado= findViewById(R.id.caixaFrase);
+
+        progressBar.setVisibility(View.GONE);
+
+
+
         btnShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,13 +66,44 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+
+
+
+
     }
 
+    public void sendNotification(View view) {
+try {
+    NotificationCompat.Builder mBuilder =
+            new NotificationCompat.Builder(this);
 
+    //Create the intent that’ll fire when the user taps the notification//
+
+    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.androidauthority.com/"));
+    PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+    mBuilder.setContentIntent(pendingIntent);
+
+    mBuilder.setSmallIcon(R.mipmap.ic_launcher_round);
+    mBuilder.setContentTitle("My notification");
+    mBuilder.setContentText(Resultado.getText());
+
+    NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+    mNotificationManager.notify(001, mBuilder.build());
+
+    Log.i("Not", "deu bom");
+
+}catch (Exception e){
+    e.printStackTrace();
+    Log.i("Not", "deu ruim");
+}
+
+    }
 
     public void getFrases() {
+        progressBar.setVisibility(View.VISIBLE);
         apiService = RetrofitBuilder.getRetrofit().create(ApiService.class);
-        //call = apiService.getPostagems();
         Call<Frases> listCall = apiService.getFrases();
         listCall.enqueue(new Callback<Frases>() {
 
@@ -72,8 +115,8 @@ public class MainActivity extends AppCompatActivity {
                     Frase.setAutor(response.body().getAutor());
                     Frase.setConteudo(response.body().getConteudo());
 
-                    TextView Resultado = findViewById(R.id.caixaFrase);
                     Resultado.setText(Frase.toString());
+                    progressBar.setVisibility(View.GONE);
                 }
             }
 
@@ -83,9 +126,40 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+    }
+
+    private void createNotificationChannel() {
+        string c;
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.app_name);
+            String description = getString(R.string.app_name);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(chanel, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 
-}
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
 
 
